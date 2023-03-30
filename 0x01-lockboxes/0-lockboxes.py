48fbb7042b91359d784b1ba16c11898ada45a39d
Phoshoko Thabang <phoshokothabang1@gmail.com>
2023-03-30 17:45:05 +0200

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''A module for working with lockboxes.
+'''
+
+
+def canUnlockAll(boxes):
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    n = len(boxes)
+    checked_boxes = set([0])
+    unchecked_boxes = set(boxes[0]).difference(set([0]))
+    while len(unchecked_boxes) > 0:
+        boxIdx = unchecked_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in checked_boxes:
+            uncheck_boxes = unchecked_boxes.union(boxes[boxIdx])
+            checked_boxes.add(boxIdx)
+    return n == len(checked_boxes)
